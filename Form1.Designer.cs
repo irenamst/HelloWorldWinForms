e5726193940c815aba530a53209c91a2ee8d70e1
Irena Staneva <irenamst@gmail.com>
2022-02-16 10:52:31 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace HelloWorldWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_hello = new System.Windows.Forms.Button();
+            this.lbl_hellomessage = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_hello
+            // 
+            this.btn_hello.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_hello.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_hello.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_hello.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_hello.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_hello.Location = new System.Drawing.Point(110, 142);
+            this.btn_hello.Name = "btn_hello";
+            this.btn_hello.Size = new System.Drawing.Size(168, 49);
+            this.btn_hello.TabIndex = 0;
+            this.btn_hello.Text = "Hello Click Me";
+            this.btn_hello.UseVisualStyleBackColor = false;
+            this.btn_hello.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // lbl_hellomessage
+            // 
+            this.lbl_hellomessage.AutoSize = true;
+            this.lbl_hellomessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbl_hellomessage.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_hellomessage.Location = new System.Drawing.Point(219, 222);
+            this.lbl_hellomessage.Name = "lbl_hellomessage";
+            this.lbl_hellomessage.Size = new System.Drawing.Size(0, 16);
+            this.lbl_hellomessage.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Change the color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 352);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl_hellomessage);
+            this.Controls.Add(this.btn_hello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_hello;
+        private System.Windows.Forms.Label lbl_hellomessage;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
